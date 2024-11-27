44a719c6ced5e9d456742b012dec9a09b6504e8a
Noman Rafiq <heyimnomee@gmail.com>
2024-11-27 17:05:19 +0500

    mobile navbar

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const addEventOnElements = (elements, eventType, callback) => {
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+};
+
+const navbar = document.querySelector('[data-navbar]');
+const navbarTogglers = document.querySelectorAll('[data-nav-toggler]');
+console.log(navbarTogglers);
+const toggleNav = () => {
+    navbar.classList.toggle('active');
+};
+
+addEventOnElements(navbarTogglers, 'click', toggleNav);
+
+// Header Animation when scrolled down to 100px header will be active
+
+const header = document.querySelector('[data-header]');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+});
